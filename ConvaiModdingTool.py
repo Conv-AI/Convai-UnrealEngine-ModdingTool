358a5396a8f1eb12634ab99869b42b07b7cf45b3
Anmol <anmol@convai.com>
2025-03-16 08:11:03 +0530

    chore: Download dependencies from gdrive

--- ConvaiModdingTool.py
+++ ConvaiModdingTool.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import sys
 
 from core.asset_manager import generate_project_name, get_asset_id, save_asset_metadata
-from core.download_utils import download_and_extract_plugin
+from core.download_utils import download_and_extract_plugin, download_plugins_from_gdrive_folder
 from core.unreal_project import build_project_structure, enable_convai_plugin_in_uproject, enable_plugin_in_uproject, extract_engine_version, get_unreal_engine_path, is_plugin_installed, is_supported_engine_version, run_unreal_build
 
 
@@ -33,16 +33,19 @@ def main():
     uproject_file = os.path.join(project_dir, f"{project_name}.uproject")
 
     build_project_structure(project_name, template_dir, project_dir, unreal_engine_path, engine_version)    
-    save_asset_metadata(project_dir, asset_id)    
-    download_and_extract_plugin(project_dir)    
-    enable_plugin_in_uproject(uproject_file, "JsonBlueprintUtilities")
-        
+    
+    download_plugins_from_gdrive_folder("11n7EZW4SBd4Ri9Q6GuXFdoLrwCZvnnwq", project_dir)
+    
+    PluginName = ["ConvAI", "ConvaiHTTP", "ConvaiPakManager", "JsonBlueprintUtilities"]
+    for It in PluginName:
+        enable_plugin_in_uproject(uproject_file, It)
+    
     run_unreal_build(unreal_engine_path, project_name, project_dir)
     
-    if not is_plugin_installed(unreal_engine_path, "Convai"):
-        print("❌ Convai plugin is not installed. Install it from the marketplace.")
-    else:
-        enable_convai_plugin_in_uproject(uproject_file)
+    # if not is_plugin_installed(unreal_engine_path, "Convai"):
+    #     print("❌ Convai plugin is not installed. Install it from the marketplace.")
+    # else:
+    #     enable_convai_plugin_in_uproject(uproject_file)
     
     input("Press Enter to exit...")  
 
