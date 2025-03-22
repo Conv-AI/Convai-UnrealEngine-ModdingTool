537dd252226d7bb0b7d591fbccbe8142844a50aa
Anmol <anmol@convai.com>
2025-03-22 17:59:02 +0530

    feat: Add content only plugin in modding project

--- ConvaiModdingTool.py
+++ ConvaiModdingTool.py
@@ -2,9 +2,9 @@ import os
 from pathlib import Path
 import sys
 
-from core.asset_manager import generate_project_name, get_asset_id, save_asset_metadata
-from core.download_utils import download_and_extract_plugin, download_plugins_from_gdrive_folder
-from core.unreal_project import build_project_structure, enable_convai_plugin_in_uproject, enable_plugin_in_uproject, extract_engine_version, get_unreal_engine_path, is_plugin_installed, is_supported_engine_version, run_unreal_build
+from core.asset_manager import trim_unique_str, get_unique_str
+from core.download_utils import download_plugins_from_gdrive_folder
+from core.unreal_project import build_project_structure, create_content_only_plugin, enable_plugin_in_uproject, extract_engine_version, get_unreal_engine_path, is_supported_engine_version, run_unreal_build
 
 
 def main():
@@ -15,12 +15,8 @@ def main():
     else:
         script_dir = os.path.dirname(os.path.abspath(__file__))
 
-    asset_id = get_asset_id()
-    if not asset_id:
-        print("Failed to fetch asset ID. Exiting.")
-        exit(1)
-
-    project_name = generate_project_name(asset_id)
+    project_name = trim_unique_str(get_unique_str())
+    plugin_name = trim_unique_str(get_unique_str())
     unreal_engine_path = get_unreal_engine_path()
 
     engine_version = extract_engine_version(unreal_engine_path)        
@@ -31,21 +27,16 @@ def main():
     template_dir = os.path.join(unreal_engine_path, "Templates", "TP_Blank")
     project_dir = os.path.join(Path(script_dir).parent, project_name)
     uproject_file = os.path.join(project_dir, f"{project_name}.uproject")
-
-    build_project_structure(project_name, template_dir, project_dir, unreal_engine_path, engine_version)    
     
+    build_project_structure(project_name, template_dir, project_dir, unreal_engine_path, engine_version)
+    create_content_only_plugin(project_dir, plugin_name)
     download_plugins_from_gdrive_folder("11n7EZW4SBd4Ri9Q6GuXFdoLrwCZvnnwq", project_dir)
     
-    PluginName = ["ConvAI", "ConvaiHTTP", "ConvaiPakManager", "JsonBlueprintUtilities"]
+    PluginName = ["ConvAI", "ConvaiHTTP", "ConvaiPakManager", "JsonBlueprintUtilities", plugin_name]
     for It in PluginName:
         enable_plugin_in_uproject(uproject_file, It)
     
     run_unreal_build(unreal_engine_path, project_name, project_dir)
-    
-    # if not is_plugin_installed(unreal_engine_path, "Convai"):
-    #     print("❌ Convai plugin is not installed. Install it from the marketplace.")
-    # else:
-    #     enable_convai_plugin_in_uproject(uproject_file)
     
     input("Press Enter to exit...")  
 
