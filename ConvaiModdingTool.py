b17445f1954598c601c94635a95ea2b39db58d29
AnmolConvai <anmol@convai.com>
2026-02-27 19:22:43 +0530

    chore: Pump version to 3.0.2

--- ConvaiModdingTool.py
+++ ConvaiModdingTool.py
@@ -11,7 +11,7 @@ from core.logger import logger, suppress_external_logging
 from core.unreal_engine_manager import UnrealEngineManager
 from core.version_manager import VersionManager
 
-TOOL_VERSION = "3.0.1"
+TOOL_VERSION = "3.0.2"
 
 def get_script_dir():
     if getattr(sys, 'frozen', False):
